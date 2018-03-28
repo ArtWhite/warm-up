08c432c20371a5ac3d20955ab9406c0906788d49
artwhite <sjobs3801@gmail.com>
2018-03-28 15:13:45 +0300

    Исправил логику

--- src/pw/artwhite/Main.java
+++ src/pw/artwhite/Main.java
@@ -10,39 +10,17 @@ public class Main {
     public static void main(String[] args) {
         Integer project1[] = {1, 1, 1, 1, 1};
         int timeOfProject1 = 3;
-        int profitOfProject1 = checkProfit(project1, timeOfProject1);
+        checkProfit(project1, timeOfProject1);
         Integer project2[] = {11, 2};
         Arrays.sort(project2, Collections.reverseOrder());
         int timeOfProject2 = 4;
-        int profitOfProject2 = checkProfit(project2, timeOfProject2);
+        checkProfit(project2, timeOfProject2);
         Integer project3[] = {8, 2, 9, 17, 4, 4, 10};
         int timeOfProject3 = 4;
-        int profitOfProject3 = checkProfit(project3, timeOfProject3);
-
-        System.out.println(profitOfProject1);
-        System.out.println(profitOfProject2);
-        System.out.println(profitOfProject3);
-
-        int[] profits = {profitOfProject1, profitOfProject2, profitOfProject3};
-
-        int maxProfit = checkBestProfit(profits);
-        System.out.println("Лучший профит - " + maxProfit + " - " + profits[maxProfit]);
+        checkProfit(project3, timeOfProject3);
     }
 
-    public static int checkBestProfit(int[] profits) {
-        int max = 0;
-        int numOfMax = 0;
-        for (int i = 0; i < profits.length; i++){
-            if(max < profits[i]){
-                max = profits[i];
-                numOfMax = i;
-                System.out.println("Профит " + i + " - " + profits[i]);
-            }
-        }
-        return numOfMax;
-    }
-
-    private static int checkProfit(Integer[] arr, int time) {
+    private static void checkProfit(Integer[] arr, int time) {
         int res = 0;
         int upTo;
         Arrays.sort(arr, Collections.reverseOrder());
@@ -54,9 +32,10 @@ public class Main {
 
         for (int i = 0; i < upTo; i++) {
             res += arr[i];
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + ", ");
         }
-        return res;
+
+        System.out.println("Результат - " + res);
     }
 
 }
