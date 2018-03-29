c396248a04d06b3cf72bc165f314eee814313bf6
artwhite <sjobs3801@gmail.com>
2018-03-29 13:36:35 +0300

    Исправил логику метода checkProfit()

--- src/pw/artwhite/Main.java
+++ src/pw/artwhite/Main.java
@@ -10,17 +10,17 @@ public class Main {
     public static void main(String[] args) {
         Integer project1[] = {1, 1, 1, 1, 1};
         int timeOfProject1 = 3;
-        checkProfit(project1, timeOfProject1);
+        System.out.println("Результат - " + checkProfit(project1, timeOfProject1));
         Integer project2[] = {11, 2};
         Arrays.sort(project2, Collections.reverseOrder());
         int timeOfProject2 = 4;
-        checkProfit(project2, timeOfProject2);
+        System.out.println("Результат - " + checkProfit(project2, timeOfProject2));
         Integer project3[] = {8, 2, 9, 17, 4, 4, 10};
         int timeOfProject3 = 4;
-        checkProfit(project3, timeOfProject3);
+        System.out.println("Результат - " + checkProfit(project3, timeOfProject3));
     }
 
-    private static void checkProfit(Integer[] arr, int time) {
+    private static int checkProfit(Integer[] arr, int time) {
         int res = 0;
         int upTo;
         Arrays.sort(arr, Collections.reverseOrder());
@@ -35,7 +35,7 @@ public class Main {
             System.out.print(arr[i] + ", ");
         }
 
-        System.out.println("Результат - " + res);
+        return res;
     }
 
 }
